51d11c9e5a8ea8c76e559b88b905137ba1fc7f5d
Long Vu <longvu727@yahoo.com>
2024-07-03 13:57:59 -0700

    add Resource struct

---
+++ resources.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"context"
+
+	"github.com/longvu727/FootballSquaresLibs/DB/db"
+	"github.com/longvu727/FootballSquaresLibs/util"
+)
+
+// Server serves gRPC requests for our banking service.
+type Resources struct {
+	config  util.Config
+	db      db.MySQL
+	context context.Context
+}
+
+func NewResources(config util.Config, db db.MySQL, context context.Context) *Resources {
+	return &Resources{
+		config:  config,
+		db:      db,
+		context: context,
+	}
+}
