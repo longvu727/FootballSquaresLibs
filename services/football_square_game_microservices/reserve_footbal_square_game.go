32691b10268c1bba6eb926904a3ca4a8af3ad1e8
Long Vu <longvu727@yahoo.com>
2024-07-14 13:08:07 -0700

    ReserveFootballSquare service

---
+++ services/football_square_game_microservices/reserve_footbal_square_game.go
@@ -0,0 +1,54 @@
+package footballsquaregameservices
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/longvu727/FootballSquaresLibs/util"
+)
+
+type ReserveFootballSquareResponse struct {
+	Acknowledged bool   `json:"acknowledged"`
+	ErrorMessage string `json:"error_message"`
+}
+
+type ReserveFootballSquareService struct {
+	GameGUID    int `json:"game_guid"`
+	RowIndex    int `json:"row_index"`
+	ColumnIndex int `json:"column_index"`
+
+	Response ReserveFootballSquareResponse
+}
+
+func (service ReserveFootballSquareService) Request(config *util.Config) (ReserveFootballSquareResponse, error) {
+	reserveFootballSquareGameResponse := ReserveFootballSquareResponse{}
+
+	client := &http.Client{}
+	serviceJson, _ := json.Marshal(service)
+
+	reserveFootballSquareGameURL := config.MICROSERVICESBASEURL["footballsquaregamemicroservices"] + "/ReserveFootballSquare"
+
+	request, err := http.NewRequest("POST", reserveFootballSquareGameURL, bytes.NewBuffer(serviceJson))
+	if err != nil {
+		return reserveFootballSquareGameResponse, err
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return reserveFootballSquareGameResponse, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		errStr := `unable to reserve FootballSquareGame`
+		reserveFootballSquareGameResponse.ErrorMessage = errStr
+
+		return reserveFootballSquareGameResponse, errors.New(errStr)
+	}
+
+	defer response.Body.Close()
+	json.NewDecoder(response.Body).Decode(&reserveFootballSquareGameResponse)
+
+	return reserveFootballSquareGameResponse, nil
+}
